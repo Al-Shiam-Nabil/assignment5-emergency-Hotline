6b4540cce8328b3e6ab8aea4dd4ddeab5d500351
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2025-08-29 00:13:47 +0600

    clear history adde

--- scripts/script.js
+++ scripts/script.js
@@ -2,55 +2,29 @@ function getId(idName) {
     return document.getElementById(idName);
 }
 
-//emargency hot line section click functionalities
-getId('card-parent').addEventListener('click', function (e) {
+function callBtnFunction(name,num){
+  //    get total coin
+        const totalCoin = parseInt(getId("total-coin").innerText);
 
-    // heart icon click functionalities
-    // get heart count 
-    const heartTotal = parseInt(getId('heart-total').innerText);
+        //    set total coin
+        const currentCoin = totalCoin - 20;
 
-    // get heart icon
-    const btnClick = e.target.className;
-    if (btnClick.includes('heart-icon-click')) {
-        // set heart count increase
-        const currentHeartTotal = heartTotal + 1;
-        getId('heart-total').innerText = currentHeartTotal;
-    }
+        if (totalCoin >= 20) {
+            alert(`📞 ${name} - ${num} এ কল করা হচ্ছে...`);
+            getId("total-coin").innerText = currentCoin;
 
+            // call history
 
-    // call button functionalites
-if(btnClick.includes('call-btn-click')){
-//   get card data
-   const serviceName= e.target.parentNode.parentNode.children[1].children[0].innerText;
-   const serviceNumber=e.target.parentNode.parentNode.children[2].children[0].innerText;
+            //get history container
+            const historyContainer = getId("history-container");
+            const newDiv = document.createElement("div");
 
-//    get total coin 
-const totalCoin=parseInt(getId('total-coin').innerText);
-
-  
-
-//    set total coin
-const currentCoin=totalCoin-20;
-
-
-if(totalCoin>=20){
-     alert(`📞 ${serviceName} - ${serviceNumber} এ কল করা হচ্ছে...`); 
-    getId('total-coin').innerText=currentCoin;
-
-    // call history
-
-    //get history container
-    const historyContainer=getId('history-container');
-    const newDiv=document.createElement('div');
-
-
-
-    newDiv.innerHTML=`
+            newDiv.innerHTML = `
        <div class="flex justify-between items-center gap-2 bg-secondary-bg p-3 mb-2 rounded-lg ">
                         <!-- left title -->
                         <div class="">
-                            <h3>${serviceName}</h3>
-                            <p class="text-secondary">${serviceNumber}</p>
+                            <h3>${name}</h3>
+                            <p class="text-secondary">${num}</p>
                         </div>
 
                         <!-- right time -->
@@ -58,38 +32,56 @@ if(totalCoin>=20){
                             <p>${new Date().toLocaleTimeString()}</p>
                         </div>
                     </div>
-    `
+    `;
 
-    // set history card
-    historyContainer.appendChild(newDiv);
-
-
-}
-else{
-      alert(`❌ আপনার পর্যাপ্ত কয়েন নাই।কল করতে কমপক্ষে ২০ কয়েন লাগবে।`);
-      return;
-}
-
-
-
-
-
-
-
-
+            // set history card
+            historyContainer.appendChild(newDiv);
+        } else {
+            alert(`❌ আপনার পর্যাপ্ত কয়েন নাই।কল করতে কমপক্ষে ২০ কয়েন লাগবে।`);
+            return;
+        }
 
 }
 
-if( btnClick.includes('call-icon')){
-    const serviceName= e.target.parentNode.parentNode.parentNode.children[1].children[0].innerText;
-    const serviceNumber=e.target.parentNode.parentNode.parentNode.children[2].children[0].innerText;
- alert(`📞 ${serviceName} - ${serviceNumber} এ কল করা হচ্ছে...`);
-}
+//emargency hot line section click functionalities
+getId("card-parent").addEventListener("click", function (e) {
+    // heart icon click functionalities
+    // get heart count
+    const heartTotal = parseInt(getId("heart-total").innerText);
 
+    // get heart icon
+    const btnClick = e.target.className;
+    if (btnClick.includes("heart-icon-click")) {
+        // set heart count increase
+        const currentHeartTotal = heartTotal + 1;
+        getId("heart-total").innerText = currentHeartTotal;
+    }
 
-})
+    // call button functionalites
+    if (btnClick.includes("call-btn-click")) {
+        //   get card data
+        const serviceName =
+            e.target.parentNode.parentNode.children[1].children[0].innerText;
+        const serviceNumber =
+            e.target.parentNode.parentNode.children[2].children[0].innerText;
+
+         callBtnFunction(serviceName,serviceNumber);
+    }
+
+    //call icon click
+    if (btnClick.includes("call-icon")) {
+        const serviceName =
+            e.target.parentNode.parentNode.parentNode.children[1].children[0]
+                .innerText;
+        const serviceNumber =
+            e.target.parentNode.parentNode.parentNode.children[2].children[0]
+                .innerText;
+        
+       callBtnFunction(serviceName,serviceNumber)
+    }
+});
 
 // clear all button functionality
-getId('clear-history-btn').addEventListener('click',function(){
-    console.log('clear btn')
-})
+getId("clear-history-btn").addEventListener("click", function () {
+    getId("history-container").innerHTML = "";
+});
